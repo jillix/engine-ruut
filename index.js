7dcce59fa2a728fec66ddc201e03f9c9d5c44a3c
Adrian Ottiker <adrian@ottiker.com>
2016-04-09 16:01:56 +0000

    make route a data handler

--- index.js
+++ index.js
@@ -9,37 +9,29 @@ exports.init = function (config, ready) {
 
     this.router = Ruut(config.routes);
 
-    if (global.addEventListener) {
-        global.addEventListener("popstate", function () {
-            console.log(arguments);
-            //checkRoute.bind(self, null));
-        });
-    }
-
     ready();
 };
 
-exports.route = function (options, stream) {
-    libob.change(options._, options);
+exports.route = function (options, data, next) {
 
-    var route = options.url = options.url || options._.url || (global.location ? global.location.pathname : '/');
-    route = this.router(route);
+    var route  = options.url || options._.url || (data ? (data.req ? data.req.url : data.url || data) : '/');
 
+    route = this.router(typeof route !== 'string' ? '/' : route);
     if (route === null) {
         route = {data: options.notDefined || this._config.notDefined || 'notFound'};
     }
 
     if (!route.data) {
-        return stream.emit('error', new Error('Engine-ruut.route: No event found.'));
+        return next(new Error('Engine-ruut.route: No event found.'));
     }
 
-    options.params = route.params || {};
-
-    // update history if url is different
-    if (global.location && options.url !== global.location.pathname) {
-        global.history.pushState(0, 0, options.url);
+    if (typeof data !== 'object') {
+        data = {};
     }
+    data.params = route.params || {};
 
-    // create event stream and pipe it to the flow chain
-    return this.flow(route.data, options);
+    // write to event stream
+    this.flow(route.data, options).write(data);
+
+    next(null, data);
 };
